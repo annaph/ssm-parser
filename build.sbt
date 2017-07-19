e6b57588482a3c23531487e67c97816ea6816603
Drazen Skobic <drazen.skobic@datalex.com>
2017-07-19 16:56:36 +0100

    ToAddress module - work in progress

--- build.sbt
+++ build.sbt
@@ -6,4 +6,5 @@ scalaVersion := "2.12.2"
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.3" % "test",
-  "org.scalacheck" %% "scalacheck" % "1.13.5" % "test")
+  "org.scalacheck" %% "scalacheck" % "1.13.5" % "test",
+  "junit" % "junit" % "4.12" % "test")
---
+++ src/test/scala/org/ssm/parser/module/ToAddressModuleSuite.scala
@@ -0,0 +1,12 @@
+package org.ssm.parser.module
+
+import org.junit.runner.RunWith
+import org.scalacheck.Arbitrary
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ToAddressModuleSuite extends FunSuite {
+
+  //implicit val arToAddress: Arbitrary[]
+}
