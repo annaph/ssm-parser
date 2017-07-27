c1585d274424494ab844baa66e25df1a17986922
Drazen Skobic <drazen.skobic@datalex.com>
2017-07-27 16:50:53 +0100

    MessageReference module - work in progress

--- src/main/scala/org/ssm/parser/module/MessageReferenceModule.scala
+++ src/main/scala/org/ssm/parser/module/MessageReferenceModule.scala
@@ -5,11 +5,15 @@ import org.ssm.parser.domain.SSMMessage
 
 import scala.util.Try
 
+import SSMModule._
+
 object MessageReferenceModule extends SSMModule {
   type R = String
   type F = String
 
-  def canProcess(input: Input): Boolean = ???
+  def canProcess(input: Input): Boolean = {
+    matchTwoGroupLine(input._2, """(^\d{2}[A-Z]{3}\d{5}[A-Z]\d{3})(.*$)""".r)
+  }
 
   def process(input: Input, state: SSMMessage): SSMMessage = ???
 
--- src/test/scala/org/ssm/parser/module/MessageReferenceModuleSuite.scala
+++ src/test/scala/org/ssm/parser/module/MessageReferenceModuleSuite.scala
@@ -8,6 +8,10 @@ import org.scalatest.prop.Checkers
 import org.scalatest.{FunSuite, Matchers}
 import org.ssm.parser.SSMProcess.Input
 import org.ssm.parser.module.MessageReferenceModule._
+import org.scalacheck.Prop.forAll
+import org.scalacheck.{Arbitrary, Gen, Prop}
+
+import scala.util.{Failure, Try, Success}
 
 @RunWith(classOf[JUnitRunner])
 class MessageReferenceModuleSuite extends FunSuite with Matchers with Checkers {
@@ -19,11 +23,45 @@ class MessageReferenceModuleSuite extends FunSuite with Matchers with Checkers {
       hasMessageRef <- arbitrary[Boolean]
       prefix <- if (hasPrefix) Gen.alphaStr else Gen.const("")
       suffix <- if (hasSuffix) Gen.alphaStr else Gen.const("")
-      middle <- if (hasMessageRef) messageRefGen else Gen.alphaStr
+      middle <- validMessageRefGen
     } yield prefix + middle + suffix
   }
 
-  def messageRefGen: Gen[String] =
+  def validMessageRefGen: Gen[String] = {
+    for {
+      firstChar <- Gen.numChar.map(_.toString)
+      secondChar <- Gen.numChar.map(_.toString)
+      thirdChar <- Gen.alphaUpperChar.map(_.toString)
+      fourthChar <- Gen.alphaUpperChar.map(_.toString)
+      fifthChar <- Gen.alphaUpperChar.map(_.toString)
+      sixthChar <- Gen.numChar.map(_.toString)
+      seventhChar <- Gen.numChar.map(_.toString)
+      eighthChar <- Gen.numChar.map(_.toString)
+      ninthChar <- Gen.numChar.map(_.toString)
+      tenthChar <- Gen.numChar.map(_.toString)
+      eleventhChar <- Gen.alphaUpperChar.map(_.toString)
+      twelfthChar <- Gen.numChar.map(_.toString)
+      thirteenthChar <- Gen.numChar.map(_.toString)
+      fourteenthChar <- Gen.numChar.map(_.toString)
+    } yield  (
+      firstChar
+        + secondChar
+        + thirdChar
+        + fourthChar
+        + fifthChar
+        + sixthChar
+        + seventhChar
+        + eighthChar
+        + ninthChar
+        + tenthChar
+        + eleventhChar
+        + twelfthChar
+        + thirteenthChar
+        + fourteenthChar
+      )
+  }
+
+  def invalidMessageRefGen: Gen[String] =
     for {
       hasFirstChar <- arbitrary[Boolean]
       hasSecondChar <- arbitrary[Boolean]
@@ -38,8 +76,37 @@ class MessageReferenceModuleSuite extends FunSuite with Matchers with Checkers {
       hasEleventhChar <- arbitrary[Boolean]
       hasTwelfthChar <- arbitrary[Boolean]
       hasThirteenthChar <- arbitrary[Boolean]
-      firstChar <- if (hasFirstChar) Gen.alphaNumChar.map(_.toString) else Gen.const("")
-    } yield firstChar
+      hasFourteenthChar <- arbitrary[Boolean]
+      firstChar <- if (hasFirstChar) Gen.numChar.map(_.toString) else Gen.const("")
+      secondChar <- if (hasSecondChar) Gen.numChar.map(_.toString) else Gen.const("")
+      thirdChar <- if (hasThirdChar) Gen.alphaUpperChar.map(_.toString) else Gen.const("")
+      fourthChar <- if (hasFourthChar) Gen.alphaUpperChar.map(_.toString) else Gen.const("")
+      fifthChar <- if (hasFifthChar) Gen.alphaUpperChar.map(_.toString) else Gen.const("")
+      sixthChar <- if (hasSixthChar) Gen.numChar.map(_.toString) else Gen.const("")
+      seventhChar <- if (hasSeventhChar) Gen.numChar.map(_.toString) else Gen.const("")
+      eighthChar <- if (hasEighthChar) Gen.numChar.map(_.toString) else Gen.const("")
+      ninthChar <- if (hasNinthChar) Gen.numChar.map(_.toString) else Gen.const("")
+      tenthChar <- if (hasTenthChar) Gen.numChar.map(_.toString) else Gen.const("")
+      eleventhChar <- if (hasEleventhChar) Gen.alphaUpperChar.map(_.toString) else Gen.const("")
+      twelfthChar <- if (hasTwelfthChar) Gen.numChar.map(_.toString) else Gen.const("")
+      thirteenthChar <- if (hasThirteenthChar) Gen.numChar.map(_.toString) else Gen.const("")
+      fourteenthChar <- if (hasFourteenthChar) Gen.numChar.map(_.toString) else Gen.const("")
+    } yield (
+      firstChar
+        + secondChar
+        + thirdChar
+        + fourthChar
+        + fifthChar
+        + sixthChar
+        + seventhChar
+        + eighthChar
+        + ninthChar
+        + tenthChar
+        + eleventhChar
+        + twelfthChar
+        + thirteenthChar
+        + fourteenthChar
+      )
 
   test("Should be able to process valid MessageReference line") {
     val input: Input = 0 -> "24MAY00144E003/REF 123/449"
@@ -60,6 +127,35 @@ class MessageReferenceModuleSuite extends FunSuite with Matchers with Checkers {
   }
 
   test("Can process line as MessageReference line") {
-    ???
+    val propMessageRefrence: Prop = forAll(messageRefAr.arbitrary) { (line: String) =>
+      val canProcessLine = canProcess(0 -> line)
+
+      if (line.size < 14)
+        canProcessLine == false
+      else {
+        val str1 = line.substring(0, 1)
+        val str2 = line.substring(5, 10)
+        val str3 = line.substring(11, 14)
+
+        println("line: " + line)
+        println("str1: " + str1)
+        println("str2: " + str2)
+        println("str3: " + str3)
+        println("---------------------------------")
+
+        Try {
+          str1.toInt
+          str2.toInt
+          str3.toInt
+        } match {
+          case Success(_) =>
+            canProcessLine == true
+          case Failure(_) =>
+            canProcessLine == false
+        }
+      }
+    }
+
+    check(propMessageRefrence)
   }
 }
