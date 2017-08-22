31b8e02d3dd93eaf8cefb6aff3f8e76e614db5cc
drazens <drazen.skobic@mail.com>
2017-08-22 22:49:52 +0100

    SSMReceiver - work in progress

--- src/test/scala/org/ssm/parser/SSMReceiverSuite.scala
+++ src/test/scala/org/ssm/parser/SSMReceiverSuite.scala
@@ -11,17 +11,124 @@ import org.ssm.parser.model.SSMMessage
 class SSMReceiverSuite extends FunSuite with Matchers {
 
   trait TestFixture {
+    val ssmMessage = SSMMessage(None, Nil)
   }
 
-  test("Previous state Start") {
+  test("Previous state Start & To Address input") {
     new TestFixture {
-      ???
+      val input: Input = 0 -> "QD AAABBCC"
+      val prev: SSMProcess = start {
+        ssmMessage -> await(SSMReceiver)
+      }
+
+      val actual: SSMProcess = SSMReceiver(Some(input), prev)
+
+      actual match {
+        case Parse(_, _) =>
+          ???
+        case _ =>
+          ???
+      }
     }
   }
 
-  test("Previous state Start & invalid input") {
-    new TestFixture {
-      ???
-    }
+  test("Previous state Start & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-ToAddress and From Address input") {
+    ???
+  }
+
+  test("Previous state Parse-ToAddress & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-FromAddress & Identifier input") {
+    ???
+  }
+
+  test("Previous state Parse-FromAddress & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-Identifier & TimeMode input") {
+    ???
+  }
+
+  test("Previous state Parse-Identifier & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-TimeMode & Reference input") {
+    ???
+  }
+
+  test("Previous state Parse-TimeMode & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-Reference & Action input") {
+    ???
+  }
+
+  test("Previous state Parse-Reference & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-Action & Flight Information input") {
+    ???
+  }
+
+  test("Previous state Parse-Action & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-FlightInformation & Period Information input") {
+    ???
+  }
+
+  test("Previous state Parse-FlightInformation & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-PeriodInformation & Period Information input") {
+    ???
+  }
+
+  test("Previous state Parse-PeriodInformation & Other Information input") {
+    ???
+  }
+
+  test("Previous state Parse-PeriodInformation & improper input") {
+    ???
+  }
+
+  test("Previous state Parse-OtherInformation & Other Information input") {
+    ???
+  }
+
+  test("Previous state Parse-OtherInformation & Sub-Action input") {
+    ???
+  }
+
+  test("Previous state Parse-OtherInformation & none input") {
+    ???
+  }
+
+  test("Previous state Parse-OtherInformation & improper input") {
+    ???
+  }
+
+  test("None input and previous state non Parse-OtherInformation") {
+    ???
+  }
+
+  test("Previous state Await") {
+    ???
+  }
+
+  test("Previous state Halt") {
+    ???
   }
 }
